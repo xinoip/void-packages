a92a76edc4237d7c3a5932791f329f9e746796b2
xinoip <xinoip@proton.me>
2026-05-01 19:12:29 +0300

    personal: disable bump.sh for now

--- personal/bump.sh
+++ personal/bump.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # "$SCRIPT_DIR/bump_zen.sh"
-"$SCRIPT_DIR/bump_brave.sh"
+# "$SCRIPT_DIR/bump_brave.sh"
+echo "does nothing for now"

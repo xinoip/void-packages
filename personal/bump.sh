eee8aec96a26df846219cb35085d3ab40d738790
xinoip <xinoip@proton.me>
2026-06-07 19:45:47 +0300

    personal: cleanup personal scripts, introduce new brave-origin bumper.

--- personal/bump.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# "$SCRIPT_DIR/bump_zen.sh"
-# "$SCRIPT_DIR/bump_brave.sh"
-echo "does nothing for now"
--- personal/bump_zen.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-exec "$SCRIPT_DIR/bump_github.sh" zen-browser zen-browser/desktop

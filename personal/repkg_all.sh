38f38ce837162398ac37c5042183b44522771b72
xinoip <xinoip@proton.me>
2026-08-03 13:55:20 +0300

    personal: yesno in repkg

--- personal/repkg_all.sh
+++ personal/repkg_all.sh
@@ -3,9 +3,20 @@ set -euo pipefail
 readonly OLD_PWD="$PWD"
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
+yesno() {
+	local -r MSG="$1"
+	local REPLY
+
+	read -n 1 -p "$MSG (y/N) " REPLY
+	printf "\n"
+
+	[[ "$REPLY" == [Yy] ]]
+}
+
 main() {
-	./repkg.sh brave-origin
-	./repkg.sh mullvad-vpn
+	yesno "Repkg brave-origin?" && ./repkg.sh brave-origin
+	yesno "Repkg mullvad-vpn?" && ./repkg.sh mullvad-vpn
+	yesno "Repkg android-studio?" && ./repkg.sh android-studio
 }
 
 main "$@"
